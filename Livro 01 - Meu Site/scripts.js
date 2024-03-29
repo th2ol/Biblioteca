c10459d856240d82b32f1170276927e60b80053e
Thiago Hansell <159553575+th2ol@users.noreply.github.com>
2024-03-29 15:33:49 -0300

    Update scripts.js

--- Livro 01 - Meu Site/scripts.js
+++ Livro 01 - Meu Site/scripts.js
@@ -5,11 +5,11 @@ for (let card of cards){
     card.addEventListener("click",function(){
         const videoId = card.getAttribute ("id");
         modal_overlay.classList.add('active')
-        modal_overlay.querySelector("iframe").src ="https://www.youtube.com/embed/${videoId}";
+        modal_overlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`;
     })
 }
 
 document.querySelector ('.close_modal').addEventListener("click",function(){
-    modal_overlay.classList.remove("active")
+    modal_overlay.classList.remove('active')
     modal_overlay.querySelector("iframe").src ="";
 })
