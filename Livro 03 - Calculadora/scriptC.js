bb912ab49e9e6eefe99e1f700a23f2c95d5d99e1
Thiago Hansell <159553575+th2ol@users.noreply.github.com>
2024-03-30 13:14:41 -0300

    Create scriptC.js

---
+++ Livro 03 - Calculadora/scriptC.js
@@ -0,0 +1,10 @@
+function insert(b) {var a = document.getElementById('resultado').innerHTML; document.getElementById('resultado').innerHTML = a + b;}
+
+function clean() {document.getElementById('resultado').innerHTML = "";}
+
+function back() {var resultado = document.getElementById('resultado').innerHTML; document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);}
+
+function calcular() {var resultado = document.getElementById('resultado').innerHTML; 
+    if(resultado){document.getElementById('resultado').innerHTML = eval(resultado)}
+    else{document.getElementById('resultado').innerHTML = "Vazio..."}
+}
